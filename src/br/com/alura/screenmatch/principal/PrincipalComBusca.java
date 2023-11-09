0bf945ed3d31240a325cb27eafb05591b6ad2092
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-09 12:47:59 -0300

    Usando interfaces, api's e bibliotecas para fazer a requisição get a uma URL estática

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,7 +1,21 @@
 package br.com.alura.screenmatch.principal;
 
-public class PrincipalComBusca {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class PrincipalComBusca {
+    //thwrows - Não vamos fazer nada e quem chamou que será delegada a tarefa.
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=c0e431f9"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        System.out.printf("");
     }
 }
