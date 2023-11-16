07e124b3c34e880ed4ee5c41df72d5985ff03060
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-16 10:12:53 -0300

    Usando replace para resolver o erro de filmes com espaço na url

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -24,13 +24,14 @@ public class PrincipalComBusca {
         var busca = leitura.nextLine();
 
         //Concatenação para tornar a busca dinâmica
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=c0e431f9";
+        //Replace trata se caso for um filme com espaçamento, ele apenas junta, pois na url não pode ter espaços
+        String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=c0e431f9";
         try {
         //Realiza a busca de acordo com o que foi digitado, criamos um client e realizamos uma requisição
         //Criando client para realizar a requisição
         HttpClient client = HttpClient.newHttpClient();
         //Construção da Requisição HTTP
-        HttpRequest request = HttpRequest.newBuilder() //Inicia a construção de uma nova requisição HTTP
+        HttpRequest request = HttpRequest.newBuilder() //Inicia a construção de uma nova requisição HTTP | newBuilder indica que é um objeto complexo, para construir ele, vai ser usado metodos
                 .uri(URI.create(endereco)) //Define a URI (Uniform Resource Identifier)
                 .build(); //Constrói a requisição HTTP
 
@@ -48,7 +49,7 @@ public class PrincipalComBusca {
         //Configurando politica de nomenclatura (pois quando resgatamos o objeto Json, a chave vem em maiscula)
         Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
         //Desserializa o JSON para um objeto Java da classe TituloOmdb
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class); //Criamos esse record, só para consumir a API
         System.out.println(meuTituloOmdb);
         //Ele não tem metodos do nosso Titulo, por isso criamos um construtor que recebe o Titulo Omdb para conseguir usarmos os metodos
 
@@ -59,7 +60,7 @@ public class PrincipalComBusca {
             System.out.println("Titulo convertido:");
             //Quando chamamos o println(x) ele automaticamente chama o toString do objeto x
             System.out.println(meuTitulo);
-            //Se acontecer esse tipo de erro, faça isso
+            //Se acontecer esse tipo de erro, faça isso'
         } catch (NumberFormatException e) {
             System.out.println("Aconteceu um erro: ");
             System.out.println(e.getMessage());
