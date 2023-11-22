8e63432c2bb279c32bbfec8c4f8f5c0258f16e8f
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-22 10:32:05 -0300

    Criando e usando o scanner

--- src/br/com/alura/viacepresolucao/Principal.java
+++ src/br/com/alura/viacepresolucao/Principal.java
@@ -1,10 +1,17 @@
 package src.br.com.alura.viacepresolucao;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         ConsultaCep consultaCep = new ConsultaCep();
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite o seu cep");
+        var cep = leitura.nextLine();
+
         try {
-            Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
             System.out.println(novoEndereco);
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
