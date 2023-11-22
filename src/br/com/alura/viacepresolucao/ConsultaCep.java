0c3f00fde2c595edacfb1b7961afd8678d637d3e
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-22 10:18:53 -0300

    Criando metodo consultaEndereco, aprendendo forma mais sucinta de extrair o corpo da resposta, e converter pro record

--- src/br/com/alura/viacepresolucao/ConsultaCep.java
+++ src/br/com/alura/viacepresolucao/ConsultaCep.java
@@ -1,5 +1,31 @@
 package src.br.com.alura.viacepresolucao;
 
-public class ConsultaCep {
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaCep {
+    public Endereco consultaEndereco(String cep) {
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json");
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco)
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Não consegui obter o endereco, a partir desse cep");
+        }
+
+        //Converte o que veio no response.body() no Endereco (record)
+        return new Gson().fromJson(response.body(), Endereco.class);
+    }
 }
